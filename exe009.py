f356c96216303146cf5d20ca69bb5689afee1f84
Monique Ferreira <86627670+MoniqueFerreira@users.noreply.github.com>
2022-07-30 10:09:32 -0300

    Delete exe009.py

--- exe009.py
+++
@@ -1,4 +0,0 @@
-#faça um programa que leia um numero qualquer e mostre em sua tela a tabuada dele de 1 á 10
-t=int(input('Digite o número da tabuada que deseja saber:_'))
-print(f' {t}x1={t*1}\n {t}x2={t*2}\n {t}x3={t*3}\n {t}x4={t*4}\n {t}x5={t*5}\n' )
-print(f' {t}x6={t*6}\n {t}x7={t*7}\n {t}x8={t*8}\n {t}x9={t*9}\n {t}x10={t*10}\n')
